162d4511f7b6b2676a4c9282a894f055e6f394be
Devaprakasam  G <dangerdeva992005@gmail.com>
2025-12-24 17:36:14 +0530

    Create C_Problem_(Assessment3)_(Problem1).c

---
+++ C_Problem_(Assessment3)_(Problem1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int eg;
+    printf("Enter an number:");
+    scanf("%d",&eg);
+    if(eg==50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+    return 0;
+}
