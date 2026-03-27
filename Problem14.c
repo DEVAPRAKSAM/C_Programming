80022c0114eeb4a4e8ddd496053ec5c3fe46e68c
Devaprakasam  G <dangerdeva992005@gmail.com>
2026-03-27 18:13:32 +0530

    Add files via upload

---
+++ Problem14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,original,first,mid,last,d,o;
+    d=1;
+    printf("Enter an number : ");
+    scanf("%d",&n);
+    original = n;
+    last = n % 10;
+    Loop:if(n>=10)
+    {
+        n = n / 10;
+        d = d * 10;
+        goto Loop;
+    }
+    last = last * d;
+    first = n;
+    mid = (original % d) / 10 ;
+    o = last + (mid*10) + first;
+    printf("%d",o);
+}
